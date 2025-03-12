7ed5851d88aac9ea53fde8f79da1ca9637cd5ba4
zenaku1000uun <cai.l.i.n.5.u.sm@gmail.com>
2025-03-13 06:28:38 +0630

    Update autoleave.py

--- maythusharmusic/plugins/misc/autoleave.py
+++ maythusharmusic/plugins/misc/autoleave.py
@@ -10,38 +10,48 @@ from maythusharmusic.utils.database import get_client, is_active_chat, is_autoen
 
 
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(900):
-            from maythusharmusic.core.userbot import assistants
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
+        from YukkiMusic.core.userbot import assistants
 
+        async def leave_inactive_chats(client):
+            left = 0
+            try:
+                async for i in client.get_dialogs():
+                    chat_type = i.chat.type
+                    if chat_type in [
+                        ChatType.SUPERGROUP,
+                        ChatType.GROUP,
+                        ChatType.CHANNEL,
+                    ]:
+                        chat_id = i.chat.id
+                        if chat_id not in [
+                            config.LOGGER_ID,
+                            -1002459775779,
+                            -1002326433754,
+                            -1001513130852,
+                            -1002409741597,
+                            -1001874904877,
+                            -1001210616755,
+                            -1002356385851,
+                        ]:
+                            if left == 20:
+                                break
+                            if not await is_active_chat(chat_id):
+                                try:
+                                    await client.leave_chat(chat_id)
+                                    left += 1
+                                except Exception:
+                                    continue
+            except Exception:
+                pass
+
+        if config.AUTO_LEAVING_ASSISTANT == str(True):
+            await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME)
+            tasks = []
             for num in assistants:
                 client = await get_client(num)
-                left = 0
-                try:
-                    async for i in client.get_dialogs():
-                        if i.chat.type in [
-                            ChatType.SUPERGROUP,
-                            ChatType.GROUP,
-                            ChatType.CHANNEL,
-                        ]:
-                            if (
-                                i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001626004802
-                                and i.chat.id != -1001876397776
-                            ):
-                                if left == 20:
-                                    continue
-                                if not await is_active_chat(i.chat.id):
-                                    try:
-                                        await client.leave_chat(i.chat.id)
-                                        left += 1
-                                    except:
-                                        continue
-                except:
-                    pass
-
-
-asyncio.create_task(auto_leave())
+                tasks.append(leave_inactive_chats(client))
+            await asyncio.gather(*tasks)
 
 
 async def auto_end():
